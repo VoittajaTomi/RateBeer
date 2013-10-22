b293cbe6375e0f6c7348f29a4fa78297a9d57eb3
user.name <tsb358@gmail.com>
2013-10-22 17:08:18 +0300

    gemfile updated for heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ratebeer::Application.config.secret_token = '45f939aa695c653d91f902c8339a2e87f5ce6fd9286fb7652d18e84714199412f14bd10c10399f5f8c3b9d2367e637aa87b3f1a7e5bb4e249054e3e430bfcf8c'
