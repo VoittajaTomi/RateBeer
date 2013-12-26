994227696fe851f5c7b6faf768b6ecc3fb3368a5
user.name <tsb358@gmail.com>
2013-12-27 00:32:45 +0200

    week3

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,14 +1,15 @@
 class Brewery < ActiveRecord::Base
   attr_accessible :name, :year
   has_many :beers
-
+  has_many :ratings, :through => :beers
   #@beers = Beer.all
 
   def average_rating
 
     avgsum = 0
     self.beers.each { |beer| avgsum +=beer.average_rating }
-    return avgsum/beers.count
+    count = self.beers.count
+    return avgsum/count
 
   end
 
