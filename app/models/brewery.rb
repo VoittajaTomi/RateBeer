67e3c81387cf444e62a8f193997666faa769bd6c
user.name <tsb358@gmail.com>
2013-12-27 00:25:59 +0200

    week3

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -2,5 +2,15 @@ class Brewery < ActiveRecord::Base
   attr_accessible :name, :year
   has_many :beers
 
+  #@beers = Beer.all
+
+  def average_rating
+
+    avgsum = 0
+    self.beers.each do |beer| angsum +=beer.average_rating
+    return avgsum/beers.count
+
+  end
+
 
 end
